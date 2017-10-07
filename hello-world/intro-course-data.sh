e2406d4c9f34dbfce1dfdf52a0684f92227a54eb
gvqualite <gvqualite@glissbid.fr>
2017-10-07 16:51:06 +0200

    Update intro-course-data.sh

--- hello-world/intro-course-data.sh
+++ hello-world/intro-course-data.sh
@@ -1,8 +1,2 @@
-echo 'package main' >> index.js
-echo 'import (' >> index.js
-echo '  "fmt"' >> index.js
-echo ')' >> index.js
-
-echo 'func main() {' >> index.js
-echo '  fmt.Fprintf(os.Stdout, "Hello World!")' >> index.js
-echo '}' >> index.js
+echo 'console.log("Hello Workd")' >> index.js
+ 
